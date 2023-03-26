12495372715145ca4138e84d3ca3e0a0200fc8b0
psychoAI123 <87062972+psychoAI123@users.noreply.github.com>
2023-03-26 15:44:57 +0530

    First Commit

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,7 +3,35 @@ import './main.css'
 
 const main = () => {
   return (
-    <div>main</div>
+   <section className='main container section'>
+
+     <div className='secTitle'>
+      <h3 className='title'>
+        Most visited Destinations
+      </h3>
+     </div>
+
+     <div className='secContent grid'>
+      {/*so guys, here we are going to use high ordfer array metod(map).
+        To do that we shall use a list if object in one array.I'm going
+        to create an array named data and from that we shall .map() array to 
+        fetch each destinations at once. I hope this will make sence to you!
+     */}
+
+      {
+        Data.map(({ id , imgsrc, destTitle, location, grade, fees,
+         description})=>{
+           return(
+            <div key={id} className='singleDestination'>
+              {/* Here it will return single id from the map array */}
+            </div>
+           )
+
+        })
+      }
+     </div>
+
+   </section>
   )
 }
 
